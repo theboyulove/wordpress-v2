0fcfd552fac07a3727531b76fc50ff2f601074ac
theboyulove <124706303+theboyulove@users.noreply.github.com>
2023-05-11 17:05:33 +0700

    upload next.config

---
+++ next.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  async redirects() {
+    return [      {        source: '/posts/:path*',        has: [          {            type: 'query',            key: 'fbclid'          }        ],
+        destination: 'https://pupperisland.com/:path*',
+        permanent: false,
+      },
+      {
+        source: '/posts/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'referer',
+          }
+        ],
+        destination: 'https://pupperisland.com/:path*',
+        permanent: false,
+      },
+      {
+        source: '/posts/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'Referer',
+            value: 'https://t.co/*',
+          },
+        ],
+        destination: 'https://pupperisland.com/:path*',
+        permanent: false,
+      },
+    ]
+  },
+  images: {
+    domains: [
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+    ],
+  },
+}
